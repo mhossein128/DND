936a0cd9f6b7c1bab63095d69816c5e11e2712fe
mhossin <mohammadimh287@gmail.com>
2022-08-16 17:28:59 +0430

    first commit

---
+++ src/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+<div className="p-5 divide-x-2 bg-slate-400 grid grid-cols-4">
+        <span className="text-center">پایان</span>
+        <span className="text-center">شروع</span>
+        <span className="text-center">مدت</span>
+        <span className="text-center">نام فیلم</span>
+      </div>
+  )
+}
+
+export default Header
